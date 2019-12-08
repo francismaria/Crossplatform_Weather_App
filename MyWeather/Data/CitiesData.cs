05335797c56becb63e3881df0e3894aec7f7ca78
francismaria <franciscomaria97@hotmail.com>
2019-12-08 11:33:30 +0000

    * CitiesData.cs: Add Cities API information
    * APICityModel.cs:

--- MyWeather/Data/CitiesData.cs
+++ MyWeather/Data/CitiesData.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
 
+using MyWeather.Model;
+
 namespace MyWeather
 {
     public class CitiesData
@@ -7,31 +9,32 @@ namespace MyWeather
         public CitiesData()
         {
         }
-        /*
-        public static List<CityInformation> Cities = new List<CityInformation>()
+
+        public static List<APICityModel> Cities = new List<APICityModel>()
         {
-            new CityInformation("aveiro", "Aveiro"),
-            new CityInformation("beja", "Beja"),
-            new CityInformation("braga", "Braga"),
-            new CityInformation("braganca","Bragança"),
-            new CityInformation("castelo-branco","Castelo Branco"),
-            new CityInformation("coimbra","Coimbra"),
-            new CityInformation("evora","Évora"),
-            new CityInformation("faro","Faro"),
-            new CityInformation("","Guarda"),
-            new CityInformation("","Leiria"),
-            new CityInformation("","Lisboa"),
-            new CityInformation("","Portalegre"),
-            new CityInformation("","Porto"),
-            new CityInformation("","Santarém"),
-            new CityInformation("","Setúbal"),
-            new CityInformation("viana-do-castelo", "Viana do Castelo"),
-            new CityInformation("","Vila Real"),
-            new CityInformation("","Viseu"),
-            new CityInformation("","Açores"),
-            new CityInformation("","Madeira")
+            new APICityModel("2742611", "Aveiro", "PT", "-8.64554", "40.644272"),
+            new APICityModel("8010436", "Beja", "PT", "-7.87773", "37.987999"),
+            new APICityModel("8010448", "Braga", "PT", "-8.43231", "41.537788"),
+            new APICityModel("8010461", "Bragança", "PT", "-6.75535", "41.756081"),
+            new APICityModel("8010473", "Castelo Branco", "PT", "-7.51313", "39.88847"),
+            new APICityModel("2740637", "Coimbra", "PT", "-8.41955", "40.205639"),
+            new APICityModel("2268406", "Évora", "PT", "-7.9", "38.566669"),
+            new APICityModel("8010516", "Faro", "PT", "-7.92774", "37.056431"),
+            new APICityModel("8010533", "Guarda", "PT", "-7.24123,", "40.529499"),
+            new APICityModel("8010549", "Leiria", "PT", "-8.79604", "39.78109"),
+            new APICityModel("2267056", "Lisboa", "PT", "-9.13333", "39"),
+            new APICityModel("6930638","Portalegre", "PT", "-7.41538", "39.297401"),
+            new APICityModel("2735943","Porto", "PT", "-8.61099", "41.149609"),
+            new APICityModel("2263480","Santarém", "PT", "-8.68333", "39.23333"),
+            new APICityModel("2262963","Setúbal", "PT", "-8.8882", "38.524399"),
+            new APICityModel("8010635", "Viana do Castelo", "PT", "-8.84674", "41.708981"),
+            new APICityModel("8010650", "Vila Real", "PT", "-7.73985", "41.300209"),
+            new APICityModel("8010671", "Viseu", "PT", "-7.91721", "40.6745"),
+
+            new APICityModel("3411865", "Açores", "PT", "-28", "38.5"),
+            new APICityModel("2593105", "Madeira", "PT", "-16.75", "32.666672")
         };
-        */
+        
     }
 
 }
---
+++ MyWeather/Model/APICityModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace MyWeather.Model
+{
+    public class APICityModel
+    {
+        class Coord
+        {
+            private string lon;
+            private string lat;
+
+            public Coord(string lon, string lat)
+            {
+                this.lon = lon;
+                this.lat = lat;
+            }
+
+            public string getLongitude()
+            {
+                return lon;
+            }
+
+            public string getLatitude()
+            {
+                return lat;
+            }
+        }
+
+        private string id;
+        private string name;
+        private string country;
+        private Coord coord;
+        
+
+        public APICityModel(string id, string name, string country, string lon, string lat)
+        {
+            this.id = id;
+            this.name = name;
+            this.country = country;
+            this.coord = new Coord(lon, lat);
+        }
+
+
+    }
+}
